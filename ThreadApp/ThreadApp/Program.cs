7d3593deb8c7c92ddafc04cf923cd7a506e4d7d1
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-02 19:49:55 -0400

    creating a thread

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadApp
 {
@@ -10,6 +11,43 @@ namespace ThreadApp
     {
         static void Main(string[] args)
         {
+
+            
+            Thread t = new Thread(secondMeth);
+            t.Name = "SecondThread";
+            t.Start();
+
+            Thread t2 = new Thread(secondMeth);
+            t2.Name = "ThirdThread";
+            t2.Start();
+
+            Console.WriteLine("Hi michael "+Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);           
+            Console.ReadLine();
+
+        }
+
+        static void secondMeth()
+        {
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+            Console.ReadLine();
         }
     }
 }
