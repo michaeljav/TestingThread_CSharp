2954e83775fda49c2ff2c9d777726adc52d55835
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-03 18:34:59 -0400

    // WITHOUT  ThreadPool     //https://www.youtube.com/watch?v=2qeDlKUsGLA&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=112&ab_channel=pildorasinformaticas     //Curso C#. Threads V. ThreadPool. Vídeo 112     //Al utilizar  threadpool  le creamos dentro  3 thread  y tenemos 5 tareas a realizar el  threadpool     //se encarga  de que cuando un thread termina de realizar su tarea el automaticamente reutiliza ese thread para comanzar a realizar la tarea siguiente.     //y podrimos reutilizar esos thread cuando terminen con otras tareas. siempre un threadpool va a consumir menos recurso.  Nota: al utilizar solo thread, se le asignaba un thread a cada tarea.

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -7,56 +7,39 @@ using System.Threading;
 
 namespace ThreadApp
 {
-    //https://www.youtube.com/watch?v=2YCIAYk63ns&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=111&ab_channel=pildorasinformaticas
-    //usando  el TaskCompletionSouce para  que una tarea comienze siempre y cuando las anteriores hayan terminado su trabajo.    Curso C#. Threads IV. Tareas completadas. Vídeo 111
-    //TaskCompletionSouce es como una condicion. El siguiente  hilo se ejecutara siempre y cuando los anteriores hilos hayan termiando su tarea.
-    //cuando se esta manjeando socket (minetras el socket no se haya cerrado no realice otro) o conexciones de base de datos (mientras este la conexion con la base de datos no realice otra tarea) 
+    // WITHOUT  ThreadPool 
+    //https://www.youtube.com/watch?v=2qeDlKUsGLA&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=112&ab_channel=pildorasinformaticas
+    //Curso C#. Threads V. ThreadPool. Vídeo 112
+    //Al utilizar  threadpool  le creamos dentro  3 thread  y tenemos 5 tareas a realizar el  threadpool 
+    //se encarga  de que cuando un thread termina de realizar su tarea el automaticamente reutiliza ese thread para comanzar a realizar la tarea siguiente.
+    //y podrimos reutilizar esos thread cuando terminen con otras tareas. siempre un threadpool va a consumir menos recurso.  Nota: al utilizar solo thread, se le asignaba un thread a cada tarea.
+
     class Program
     {
+        public static int v;
         static void Main(string[] args)
         {
-
-            var tareaTerminada = new TaskCompletionSource<bool>();
-
-            var hilo1 = new Thread(() =>
+            
+            for (int i = 0; i < 500; i++)
             {
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine("Hilo 1");
-                    Thread.Sleep(1000);
-                }
+                v = i;
+                Thread t = new Thread(EjecutarTarea);
+                t.Start();
+            }
 
-                tareaTerminada.TrySetResult(true);
-            });
-
-            var hilo2 = new Thread(() =>
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine("Hilo 2");
-                    Thread.Sleep(1000);
-                }
-
-            });
-
-            var hilo3 = new Thread(() =>
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine("Hilo 2");
-                    Thread.Sleep(1000);
-                }
-                tareaTerminada.TrySetResult(true);
-
-            });
-
-            hilo1.Start();
-            hilo2.Start();
-            var resultado = tareaTerminada.Task.Result;
-           
-            hilo3.Start();
-
+            Console.WriteLine();
         }
+
+        static void EjecutarTarea()
+        {
+            Console.WriteLine($"Thread n: {Thread.CurrentThread.ManagedThreadId} ha comenzado su tarea  HiloCount: {v}");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"Thread n: {Thread.CurrentThread.ManagedThreadId} ha terminado su tarea HiloCount: {v} ");
+        }
+
+
 
       
     }
