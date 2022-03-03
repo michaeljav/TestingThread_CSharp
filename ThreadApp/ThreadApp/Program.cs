00eec74951713092606a854fcc2564d5967767a4
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-02 20:51:22 -0400

    Using  (lock) in thread csharp

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -7,51 +7,67 @@ using System.Threading;
 
 namespace ThreadApp
 {
+    //https://www.youtube.com/watch?v=XplEHgqBQhE&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=111&ab_channel=pildorasinformaticas
     class Program
     {
         static void Main(string[] args)
         {
-            //sync (join) and lock (lock) threads
+            CuentaBancaria CuentaFamilia = new CuentaBancaria(2000);
 
-            Thread t = new Thread(secondMeth);
-            t.Name = "SecondThread";
-            t.Start();
-            t.Join();
 
-            Thread t2 = new Thread(secondMeth);
-            t2.Name = "ThirdThread";
-            t2.Start();
-            t2.Join();
+            Thread[] hilosPersonas = new Thread[15];
 
-            Console.WriteLine("Main Method " + Environment.ProcessorCount);
-            Console.WriteLine("Hi michael "+Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);           
-            Console.ReadLine();
+            for (int i = 0; i < 15; i++)
+            {
+                Thread t = new Thread(CuentaFamilia.VamosRetirarEfectivo);
+                t.Name = i.ToString();
+                hilosPersonas[i] = t;
+            }
+            foreach (Thread t in hilosPersonas) t.Start();
+
+           // Console.ReadLine();
 
         }
 
-        static void secondMeth()
+        class CuentaBancaria
         {
-            Console.WriteLine("Second Method " + Thread.CurrentThread.ManagedThreadId);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            Console.WriteLine("Hi michael " + Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
-            
+            private Object bloquearSaldoPositivo = new object();
+            double Saldo { set; get; }
+            public CuentaBancaria(double Saldo)
+            {
+                this.Saldo = Saldo;
+            }
+
+            public double RetirarEfectivo(double cantidad)
+            {
+                if ((Saldo - cantidad) < 0)
+                {
+                    Console.WriteLine($"I'm sorry ${Saldo} peso in the account left  Hilo: {Thread.CurrentThread.Name}");
+                    return Saldo;
+                }
+
+                lock (bloquearSaldoPositivo) { 
+                    if (Saldo >= cantidad)
+                    {
+                        Console.WriteLine("Retirado {0} y queda {1} en la cuenta. {2}", cantidad, (Saldo -cantidad), Thread.CurrentThread.Name);
+                        Saldo -= cantidad;
+                    }
+
+                return Saldo;
+
+                }
+
+            }
+
+            public void VamosRetirarEfectivo()
+            {
+                Console.WriteLine("Esta sacando dinero el hilo: {0}", Thread.CurrentThread.Name);
+                for (int i = 0; i < 4; i++)
+                {
+                    RetirarEfectivo(500);
+                }
+               
+            }
         }
     }
 }
