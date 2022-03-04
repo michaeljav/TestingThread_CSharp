53e1ee1881e448a91a8206ca2a5725e2dde372d7
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-03 20:58:30 -0400

    //   TASK Run     //https://www.youtube.com/watch?v=x0gO4FoLJgg&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=114&ab_channel=pildorasinformaticas     //Curso C#. TASK II. Vídeo 114     //manera mas abreviada de crear hilos

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -7,37 +7,38 @@ using System.Threading;
 
 namespace ThreadApp
 {
-    //   TASK son recomendados. 
-    //https://www.youtube.com/watch?v=v3yd_Ctl6PQ&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=113&ab_channel=pildorasinformaticas
-    //Curso C#. TASK I. Vídeo 113
-    // las task son capaces de gestionar el numero de thread que se utilizan en cada momento. 
-    //the tasks are able to manage the number of threads that are used at any given time.
-    //Una task es algo futuro o una promesa 
-    //Thread es la herramienta para conseguir realizar esa promesa.
+    //   TASK Run
+    //https://www.youtube.com/watch?v=x0gO4FoLJgg&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=114&ab_channel=pildorasinformaticas
+    //Curso C#. TASK II. Vídeo 114
+    //manera mas abreviada de crear hilos
+
     class Program
     {
         
         static async  Task Main(string[] args)
         {
 
-            Task task = new Task(EjecutarTarea);
-            task.Start();
+            //Task task = new Task(EjecutarTarea);
+            //task.Start();
 
-            Task task2 = new Task(() =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    var miThread = Thread.CurrentThread.ManagedThreadId;
+            Task tarea = Task.Run(() => EjecutarTarea());
+            Task tarea2 = Task.Run(() => EjecutarOtraTarea());
 
-                    Thread.Sleep(1000);
+            //Task task2 = new Task(() =>
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        var miThread = Thread.CurrentThread.ManagedThreadId;
 
-                    Console.WriteLine($"Tarea  corresponde al Thread : " + miThread + " EJecutandose desde el main");
+            //        Thread.Sleep(1000);
 
-                }
+            //        Console.WriteLine($"Tarea  corresponde al Thread : " + miThread + " EJecutandose desde el main");
 
-            });
-            task2.Start();
-            await task;
+            //    }
+
+            //});
+            //task2.Start();
+            //await task;
             Console.WriteLine("Finished main");
 
             Console.ReadLine();
@@ -46,7 +47,7 @@ namespace ThreadApp
         static void EjecutarTarea( )
         {
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {
                 var miThread = Thread.CurrentThread.ManagedThreadId;
 
@@ -54,15 +55,25 @@ namespace ThreadApp
 
                 Console.WriteLine($"Esta vuelta de bucle corresponde al Thread : " +miThread);
             }
-         
+                    
+        }
 
-           
+        static void EjecutarOtraTarea()
+        {
 
-           
+            for (int i = 0; i < 10; i++)
+            {
+                var miThread = Thread.CurrentThread.ManagedThreadId;
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"Esto es otra tareaEsta y esta vuelta de bucle corresponde al Thread : " + miThread);
+            }
+
         }
 
 
 
-      
+
     }
 }
