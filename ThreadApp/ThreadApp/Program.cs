cbfcb0a6359f807a4a28c7e06ca04c7350267eb8
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-04 16:46:13 -0400

    //   Cancel Task     //https://www.youtube.com/watch?v=Gl-DQxoDtZ4&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=118&ab_channel=pildorasinformaticas     //Curso C#. Cancelación de Task. Vídeo 117     //cacelation of task

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -8,36 +8,86 @@ using System.Threading;
 namespace ThreadApp
 {
     //   Parallel.for
-    //https://www.youtube.com/watch?v=LwxTVdyUXdM&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=119
-    //Curso C#. Clase Parallel. Vídeo 116
-    //Crea hilos para poder hacer la tarea mas rapido
+    //https://www.youtube.com/watch?v=Gl-DQxoDtZ4&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=118&ab_channel=pildorasinformaticas
+    //Curso C#. Cancelación de Task. Vídeo 117
+    //cacelation of task
 
     class Program
     {
         private static int accumulador = 0;
         static async  Task Main(string[] args)
         {
+            /*tESTING THE RETURN AND BREAK*/
+            /* for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("inside for");
+                if (i == 0)
+                {
+                    //BREAK GO OUT FROM THE BLOCK AND CONTINUE THE EXECUTION
+                    //RETURN GO OUT FROM THE FUNCTION.
+                    break;
+                }
+                Console.WriteLine("after if");
 
-            Parallel.For(0, 100, EjecutarTarea);
+            }
+            Console.WriteLine("after for");
+            */
+
+            //CancellationTokenSource object points to the token that should interrupt the task
+            CancellationTokenSource PointerToCancelTask = new CancellationTokenSource();
+
+            //CancellationToken object that cancel the task or thread
+            CancellationToken cancellationToken = PointerToCancelTask.Token;
+
+
+            Task tarea = Task.Run(() => EjecutarTarea(cancellationToken));
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                accumulador += 30;
+
+                Thread.Sleep(1000);
+
+                if (accumulador > 100)
+                {
+                   PointerToCancelTask.Cancel();
+
+                    break;
+                }
+            }
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Valor del acumulador: " + accumulador);
 
             Console.ReadLine();
         }
 
 
       
-        static void EjecutarTarea( int dato )
+        static void EjecutarTarea( CancellationToken tokenCancelation )
         {
-            Console.WriteLine($"Acumulador vale {accumulador}. Tarea realizada por el hilo {Thread.CurrentThread.ManagedThreadId}");
-            if ((accumulador % 2 ) == 0)
+            for (int i = 0; i < 100; i++)
             {
-                accumulador += dato;
-                Thread.Sleep(100);
+                accumulador++;
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"Ejecutando tarea el thread: {threadId}");
+
+                Console.WriteLine(accumulador);
+                //if this token received a canceletion request 
+                if (tokenCancelation.IsCancellationRequested)
+                {
+                    accumulador = 0;
+                    return;                   
+                }
             }
-            else
-            {
-                accumulador -= dato;
-                Thread.Sleep(100);
-            }
+
+            
+           
 
            
                     
