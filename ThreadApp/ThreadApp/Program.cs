27dcd798e19e50917e82262c067c7060548408bc
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-03 20:45:52 -0400

    //   TASK son recomendados.     //https://www.youtube.com/watch?v=v3yd_Ctl6PQ&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=113&ab_channel=pildorasinformaticas     //Curso C#. TASK I. Vídeo 113     // las task son capaces de gestionar el numero de thread que se utilizan en cada momento.     //the tasks are able to manage the number of threads that are used at any given time.     //Una task es algo futuro o una promesa     //Thread es la herramienta para conseguir realizar esa promesa.

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -7,45 +7,58 @@ using System.Threading;
 
 namespace ThreadApp
 {
-    //   ThreadPool  :It is important to note that threads are reused in ThreadPool 
-    //https://www.youtube.com/watch?v=2qeDlKUsGLA&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=112&ab_channel=pildorasinformaticas
-    //Curso C#. Threads V. ThreadPool. Vídeo 112
-    //Al utilizar  threadpool  le creamos dentro  3 thread  y tenemos 5 tareas a realizar el  threadpool 
-    //se encarga  de que cuando un thread termina de realizar su tarea el automaticamente reutiliza ese thread para comanzar a realizar la tarea siguiente.
-    //y podrimos reutilizar esos thread cuando terminen con otras tareas. siempre un threadpool va a consumir menos recurso.  Nota: al utilizar solo thread, se le asignaba un thread a cada tarea.
-
+    //   TASK son recomendados. 
+    //https://www.youtube.com/watch?v=v3yd_Ctl6PQ&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=113&ab_channel=pildorasinformaticas
+    //Curso C#. TASK I. Vídeo 113
+    // las task son capaces de gestionar el numero de thread que se utilizan en cada momento. 
+    //the tasks are able to manage the number of threads that are used at any given time.
+    //Una task es algo futuro o una promesa 
+    //Thread es la herramienta para conseguir realizar esa promesa.
     class Program
     {
         
-        static void Main(string[] args)
+        static async  Task Main(string[] args)
         {
-            
-            for (int i = 0; i < 500; i++)
+
+            Task task = new Task(EjecutarTarea);
+            task.Start();
+
+            Task task2 = new Task(() =>
             {
+                for (int i = 0; i < 100; i++)
+                {
+                    var miThread = Thread.CurrentThread.ManagedThreadId;
 
-                /*  
-                 *  //Usando hilos  por cada tarea  crea un hilo mientras que con el  threadpool 
-                 *  //se crean menos hilos y se reusan. La maquina los gestiona.
-                 *  //Tardan mas las tareas en ejecutarse porque no  tiene un hilo por cada tarea, sino que 
-                 *  //crea una x cantidad de hilos  y esos los reusa.
-                 *  
-                 *  Thread t = new Thread(EjecutarTarea);
-                  t.Start();*/
+                    Thread.Sleep(1000);
 
-                ThreadPool.QueueUserWorkItem(EjecutarTarea, i);
-            }
+                    Console.WriteLine($"Tarea  corresponde al Thread : " + miThread + " EJecutandose desde el main");
+
+                }
+
+            });
+            task2.Start();
+            await task;
+            Console.WriteLine("Finished main");
 
             Console.ReadLine();
         }
 
-        static void EjecutarTarea(Object o )
+        static void EjecutarTarea( )
         {
-            int nTarea = (int)o;
-            Console.WriteLine($"Thread n: {Thread.CurrentThread.ManagedThreadId} ha comenzado la tarea  n  "+nTarea);
 
-            Thread.Sleep(1000);
+            for (int i = 0; i < 100; i++)
+            {
+                var miThread = Thread.CurrentThread.ManagedThreadId;
 
-            Console.WriteLine($"Thread n: {Thread.CurrentThread.ManagedThreadId} ha terminado  la tarea  n  " + nTarea);
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"Esta vuelta de bucle corresponde al Thread : " +miThread);
+            }
+         
+
+           
+
+           
         }
 
 
